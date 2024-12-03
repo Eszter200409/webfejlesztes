c6ad688ad7c0d6f7a48748a1bb13503a834913e2
Eszter200409 <gall.eszti.28@gmail.com>
2024-12-03 15:59:51 +0100

    Add files via upload

---
+++ kerdoiv.js
@@ -0,0 +1,15 @@
+pont=0;
+function ellenorzes(){
+    if (f1=>jo1){
+        pont=pont+1;
+    }
+    if (f2=>jo2){
+        pont=pont+1;
+    }
+    if (f3=>jo3){
+        pont=pont+1;
+    }
+    if (f4=>jo4){
+        pont=pont+1;
+    }
+}
